04985d991d109782506b7537bf4d946a9a220b4e
PeteM <github@thrap.co.uk>
2014-05-21 22:30:22 +0100

    new 404 template

---
+++ 404.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+			
+			<div id="content">
+
+				<div id="inner-content" class="row clearfix">
+			
+					<div id="main" class="large-8 medium-8 columns first clearfix" role="main">
+
+						<article id="post-not-found" class="hentry clearfix">
+						
+							<header class="article-header">
+								<h1><?php _e("404 - Page Not Found", "jointstheme"); ?></h1>
+							</header> <!-- end article header -->
+					
+							<section class="entry-content">
+								<p style="margin-bottom:10em"><?php _e("Sorry, the page you were looking for was not found, try returning to the <a href='/'>home page</a>", "jointstheme"); ?></p>
+								
+							</section> <!-- end article section -->
+
+					
+						</article> <!-- end article -->
+			
+					</div> <!-- end #main -->
+
+				</div> <!-- end #inner-content -->
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
