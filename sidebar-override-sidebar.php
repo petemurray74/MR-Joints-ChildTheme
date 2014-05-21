a67d7e6e7c2b05c0951fb3ae09cc00f96a040703
PeteM <github@thrap.co.uk>
2014-05-21 14:06:06 +0100

    override sidebar
    appears on pages specified by meta key

---
+++ sidebar-override-sidebar.php
@@ -0,0 +1,25 @@
+<div id="sidebar1" class="sidebar large-4 medium-12 columns" role="complementary">
+
+   <?php if ($override_sidebar=get_post_meta( get_the_ID(), 'sidebar', true ))
+	{
+	echo($override_sidebar);
+	}
+	else {get_sidebar();} 
+	?>
+
+
+	<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
+
+		<?php dynamic_sidebar( 'sidebar1' ); ?>
+
+	<?php else : ?>
+
+	<!-- This content shows up if there are no widgets defined in the backend. -->
+						
+	<div class="alert help">
+		<p><?php _e("Please activate some Widgets.", "jointstheme");  ?></p>
+	</div>
+
+	<?php endif; ?>
+
+</div>
