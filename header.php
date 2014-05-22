3a5f2b858e250cf6db2495f48c0edcc9dd36a50f
PeteM <github@thrap.co.uk>
2014-05-22 20:44:03 +0100

    adding favicons and apple icons

--- header.php
+++ header.php
@@ -19,13 +19,12 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 		<!-- icons & favicons -->
-		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico">
 		<!--[if IE]>
-			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+			<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico">
 		<![endif]-->
 		<meta name="msapplication-TileColor" content="#f01d4f">
-		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+		<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/images/win8-tile-icon.png">
 
   	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
