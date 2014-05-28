93bae7cedc60286ec8f0ddcb7d6832ea7b7ca0b4
PeteM <github@thrap.co.uk>
2014-05-28 17:43:13 +0100

    adding content to footer

--- footer.php
+++ footer.php
@@ -1,13 +1,25 @@
-					<footer class="footer" role="contentinfo">
+<?php global $mr_stylesheet_dir; ?>				
+				<footer class="footer" role="contentinfo">
 					
 						<div id="inner-footer" class="row clearfix">
 						
-							<div class="large-12 medium-12 columns">
-								<nav role="navigation">
-								<p>A consistent footer with contact information and logos of membership organisations</p>
-		    						<?php joints_footer_links(); ?>
-		    					</nav>
+							<div class="large-5 medium-5 columns">
+								<p><strong>Michael Russell and Company</strong><br>
+								PO Box 449<br>
+								Manchester<br>
+								M45 0DD</p>
+								<p>
+								Tel: 0161 773 6194<br>
+								Fax: 0161 773 6194<br>
+								Email: c.hurst@michaelrusselltrafford.co.uk<br>
+								</p>
+		    						<?php //joints_footer_links(); ?>
 		    				</div>
+							<div class="large-7 medium-7 columns">
+							<p><img class="icpa-logo" src="<?php echo $mr_stylesheet_dir;?>/images/icpa-logo.png"><br>
+							An Independent Certified Practising Accountant
+							</p>
+							</div>
 			               
 			                <div class="large-12 medium-12 columns">		
 								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
