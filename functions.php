5c31d375ee12831798eb6d7a7c2002fab6ebefe4
PeteM <github@thrap.co.uk>
2014-06-03 17:02:23 +0100

    add anti spam shortcode

--- functions.php
+++ functions.php
@@ -16,10 +16,20 @@ $mr_stylesheet_dir = get_stylesheet_directory_uri();
 		}
 	}
 	
-	// add retina image support
-	require_once(get_stylesheet_directory().'/library/retina2x.php'); 
+// add retina image support
+require_once(get_stylesheet_directory().'/library/retina2x.php'); 
 	
-	//stop styles being added again
-    remove_action('wp_enqueue_scripts', 'osc_add_frontend_efs_scripts',-100);
-    remove_action('wp_enqueue_scripts', 'efs_osc_add_dynamic_css',100);
+//stop styles being added again
+remove_action('wp_enqueue_scripts', 'osc_add_frontend_efs_scripts',-100);
+remove_action('wp_enqueue_scripts', 'efs_osc_add_dynamic_css',100);
+
+// creates [email] shortcode
+function ea_email_encode_function( $atts, $content = null ) {
+     extract( shortcode_atts( array(
+          'address' => 'email',
+     ), $atts ) );
+
+     return '<a href="mailto:' . antispambot(esc_attr($address)) . '">' . $content . '</a>';
+}
+add_shortcode('email', 'ea_email_encode_function');
 	
